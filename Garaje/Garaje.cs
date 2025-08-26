b178942ea1e0c87b1b2f2d1391a1f8f1b3d1a8a6
Mike-joseph23 <202302609@umh.edu.hn>
2025-08-25 22:14:21 -0600

    Agregar archivos de proyecto.

---
+++ Garaje/Garaje.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaje
+{
+    internal class Garaje
+    {
+        public int numeroGaraje;
+        public string descripcion;
+        public int capacidadEspacios;
+        public int espaciosOcupados;
+
+        public object EspaciosOcupados { get; private set; }
+        public object CapacidadEspacios { get; private set; }
+
+        public Garaje(int numero, string descripcion, int capacidad, int ocupados)
+        {
+            this.numeroGaraje = numero;
+            this.descripcion = descripcion;
+            this.capacidadEspacios = capacidad;
+            this.espaciosOcupados = ocupados;
+        }
+
+
+        public int EspaciosDisponibles()
+
+        {
+            return capacidadEspacios - espaciosOcupados;
+        }
+        public void ActualizarEspaciosOcupados(int solicitados)
+        {
+            EspaciosOcupados = EspaciosOcupados;
+        }
+    }
+}
---
+++ Garaje/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Garaje miGaraje = new Garaje(1, "Garaje A", 50, 20);
+
+            Console.WriteLine("Espacios disponibles: " + miGaraje.EspaciosDisponibles());
+
+            miGaraje.ActualizarEspaciosOcupados(5);
+
+
+            Console.WriteLine("Espacios disponibles despues de actualizar: " + miGaraje.EspaciosDisponibles());
+        }
+    }
+}
